9b5ee525eab8c82be72b48bc5e6ed3299952da5d
khalid-34 <mess-34@hotmail.fr>
2021-01-21 16:04:25 +0100

    ajout des logs

--- tam2.py
+++ tam2.py
@@ -5,11 +5,23 @@ import urllib.request
 #import csv
 from time import *
 
-    
+import logging
+import time
+
+
+
+logging.basicConfig(
+    filename='prog.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',
+    )   
+
+logging.info('Mon programme demarre')
 
 def clear_rows(cursor):
     cursor.execute("""DELETE FROM infoarret""")
-
+    logging.info('clear_rows')
 
 def insert_csv_row(csv_row, cursor):   
     cursor.execute("""INSERT INTO infoarret VALUES (?,?,?,?,?,?,?,?,?,?,?) """,
@@ -25,9 +37,11 @@ def load_csv(path, cursor):
         while line:
             insert_csv_row(line, cursor)
             line = f.readline()
+    logging.info('load_csv')
 
 def remove_table(cursor):
     cursor.execute("""DROP TABLE IF EXISTS infoarret""")
+    logging.info('remove_table')
 
 def create_schema(cursor):
     cursor.execute("""CREATE TABLE IF NOT EXISTS "infoarret" (
@@ -43,8 +57,10 @@ def create_schema(cursor):
     "delay_sec"	INTEGER,
     "dest_arr_code"	INTEGER
     );""")
+    logging.info('create_schema')
 
 def temps_arrive(horaire):
+    logging.info('temps_arrive')
     return strftime('%M min %S sec', gmtime(horaire))
 
 def time_tram(database, cursor):  # argument 'time' tram
@@ -54,6 +70,7 @@ def time_tram(database, cursor):  # argument 'time' tram
     """, (args.station, args.destination, args.ligne))
     for row in cursor:
         print(f'Prochain passage de la ligne {row[4]} passant à {row[3]} vers {row[5]} départ dans : {temps_arrive(row[9])}')
+    logging.info('time_tram')
 
 def next_tram(database, cursor):  # argument 'next' tram
     cursor.execute("""
@@ -62,11 +79,12 @@ def next_tram(database, cursor):  # argument 'next' tram
     """, (args.station, ))
     for row in cursor:
         print(f'Ligne {row[4]} vers {row [5]} départ dans : {temps_arrive(row[9])}')
-
+    logging.info('next_tram')
 
 def update_db():
     csv_url = 'https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv'
     urllib.request.urlretrieve(csv_url, 'tam.csv')
+    logging.info('update_db')
 
 parser = argparse.ArgumentParser("Script to interact with data from the TAM API")
 parser.add_argument("-l", "--ligne", type=str, help="entre une ligne de tram")
@@ -75,6 +93,7 @@ parser.add_argument("-s", "--station", type=str, help="entre une station" )
 parser.add_argument("-c", "--currentdb", type=str, help="Use exciting database")
 parser.add_argument("action", help="next tram or time tram")
 parser.add_argument("-f", "--fichier", action='store_true', help="créer un fichier" )
+logging.info('parser')
 
 args = parser.parse_args()
 
@@ -86,25 +105,30 @@ def generer_doc():
     fichier = fichier.writelines()
     for lignes in fichier:
         print(lignes)
+    logging.info('generer_doc')
 
 
 def main():
     if not args.action:
         print("Error : il manque un argument action ('time' ou 'next')")
+        logging.warning('il manque un argument action')
         return 1
     if args.action == 'time': 
         if not args.station or not args.ligne or not args.destination: 
             print("Error: il manque la ligne et/ou la station et/ou la destination dans les arguments")
+            logging.warning('Error: il manque la ligne et/ou la station et/ou la destination dans les arguments')
             return 1
     if args.action == 'next':
         if not args.station: 
             print("Error: il manque la station dans les arguments")
+            logging.warning('Error: il manque la station dans les arguments')
             return 1
 
     conn = sqlite3.connect('tam.db')
 
     if not conn: # si format ne convient pas, si la base est corrompue...etc
         print("Error : could not connect to database ")
+        logging.warning('Error : could not connect to database')
         return 1
 
     c = conn.cursor()
@@ -124,6 +148,7 @@ def main():
         next_tram('tam.db', c)
     conn.commit()
     conn.close()
+    logging.info('fin du programme')
 
     
 
