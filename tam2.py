a748305e2432c7cc02216ea64abcec33c91c923a
Nico34000 <prodhomme.nicolas@hotmail.fr>
2021-01-21 22:28:07 +0100

    Ajout de la fonction -f , doctring, tout fonctionnel manque logging et PEP8

--- tam2.py
+++ tam2.py
@@ -2,33 +2,47 @@ import sqlite3
 import argparse
 import sys
 import urllib.request
-#import csv
 from time import *
 
-import logging
-import time
 
 
-
-logging.basicConfig(
-    filename='prog.log',
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s - %(message)s',
-    datefmt='%d/%m/%Y %H:%M:%S',
-    )   
-
-logging.info('Mon programme demarre')
+    
 
 def clear_rows(cursor):
-    cursor.execute("""DELETE FROM infoarret""")
-    logging.info('clear_rows')
+    """ This function does : Delete lines in table 'infoarret' for refresh the
+    the line in the column before update.
+    
+    cursor : Cursor is the bridge that connects Python and SQlite databases
+    and works under SQLite3 built-in package and It will be use to execute SQL
+    commands. It acts like a position indicator and will be mostly use to 
+    retrieve data.
 
-def insert_csv_row(csv_row, cursor):   
+    """
+    cursor.execute("""DELETE FROM infoarret""")
+
+
+def insert_csv_row(csv_row, cursor):
+    """ This function insert values in table 'infoarret' 
+    
+    cursor : It acts like a position indicator and will be mostly use to 
+    retrieve data.
+
+    csv_row : retrieve the lines on the csv file.
+
+    """   
     cursor.execute("""INSERT INTO infoarret VALUES (?,?,?,?,?,?,?,?,?,?,?) """,
                    csv_row.strip().split(";"))
 
 
 def load_csv(path, cursor):
+    """ This function load and read the csv file, and insert row in db file.
+
+    cursor : It acts like a position indicator and will be mostly use to 
+    retrieve data.
+
+    path : Source of the csv file.
+
+    """
     with open(path, "r") as f:
         # ignore the header
         f.readline()
@@ -37,13 +51,27 @@ def load_csv(path, cursor):
         while line:
             insert_csv_row(line, cursor)
             line = f.readline()
-    logging.info('load_csv')
 
 def remove_table(cursor):
+    """This function remove table 'infoarret' if exist,
+    for remove before update
+
+    cursor : It acts like a position indicator and will be mostly use to 
+    retrieve data.
+    
+    
+    """
     cursor.execute("""DROP TABLE IF EXISTS infoarret""")
-    logging.info('remove_table')
 
 def create_schema(cursor):
+    """ This function create table 'infoarret' if not exist
+
+    this table contains 11 columns and determinate the type.
+
+    cursor : It acts like a position indicator and will be mostly use to 
+    retrieve data.
+
+    """
     cursor.execute("""CREATE TABLE IF NOT EXISTS "infoarret" (
     "course"	INTEGER,
     "stop_code"	TEXT,
@@ -57,34 +85,70 @@ def create_schema(cursor):
     "delay_sec"	INTEGER,
     "dest_arr_code"	INTEGER
     );""")
-    logging.info('create_schema')
 
 def temps_arrive(horaire):
-    logging.info('temps_arrive')
+    """This function return time (sec) to time (min,sec)"""
     return strftime('%M min %S sec', gmtime(horaire))
 
 def time_tram(database, cursor):  # argument 'time' tram
+    """ This function configure the argument 'time'
+    
+    this function request
+    the database for recuperate the line in the column 
+    (stop_name , trip_headsign and route_short_name) and return the request
+
+    cursor : It acts like a position indicator and will be mostly use to 
+    retrieve data.
+
+    database : Search in the SQlite database
+    
+    """
     cursor.execute("""
     SELECT * FROM infoarret
     WHERE stop_name = ? AND trip_headsign = ? AND route_short_name = ?
     """, (args.station, args.destination, args.ligne))
     for row in cursor:
-        print(f'Prochain passage de la ligne {row[4]} passant à {row[3]} vers {row[5]} départ dans : {temps_arrive(row[9])}')
-    logging.info('time_tram')
+        if args.fichier:
+            time_passage = str(f'Prochain passage de la ligne {row[4]} passant à {row[3]} vers {row[5]} départ dans : {temps_arrive(row[9])}\n')
+            with open("passage.txt", "a", encoding='utf8') as f :
+                f.writelines(time_passage)
+        else:
+            print(f'Prochain passage de la ligne {row[4]} passant à {row[3]} vers {row[5]} départ dans : {temps_arrive(row[9])}')
 
 def next_tram(database, cursor):  # argument 'next' tram
+    """The function configure the argument 'next'
+    
+    this function request the
+    database for recuperate line in the column stop_name,delay_sec and
+    route_short_name.
+    Then it returns the next passes in min,sec, the line and the direction. 
+
+    cursor : It acts like a position indicator and will be mostly use to 
+    retrieve data.
+    
+    database : Search in the SQlite database.
+
+    
+    """
     cursor.execute("""
     SELECT * FROM infoarret
     WHERE stop_name = ?
     """, (args.station, ))
     for row in cursor:
-        print(f'Ligne {row[4]} vers {row [5]} départ dans : {temps_arrive(row[9])}')
-    logging.info('next_tram')
+        if args.fichier:
+            passage = str(f'Ligne {row[4]} vers {row [5]} départ dans : {temps_arrive(row[9])}\n')
+            with open("passage.txt", "a", encoding='utf8') as f :
+                f.writelines(str(passage))
+        else:        
+            print(f'Ligne {row[4]} vers {row [5]} départ dans : {temps_arrive(row[9])}')
+
 
 def update_db():
+    """This function, retrieve the csv from url and download this csv file
+    
+    """
     csv_url = 'https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv'
     urllib.request.urlretrieve(csv_url, 'tam.csv')
-    logging.info('update_db')
 
 parser = argparse.ArgumentParser("Script to interact with data from the TAM API")
 parser.add_argument("-l", "--ligne", type=str, help="entre une ligne de tram")
@@ -93,42 +157,38 @@ parser.add_argument("-s", "--station", type=str, help="entre une station" )
 parser.add_argument("-c", "--currentdb", type=str, help="Use exciting database")
 parser.add_argument("action", help="next tram or time tram")
 parser.add_argument("-f", "--fichier", action='store_true', help="créer un fichier" )
-logging.info('parser')
 
 args = parser.parse_args()
 
 
 
-def generer_doc():
-
-    fichier = open('passages.txt', 'w', encoding='utf8')
-    fichier = fichier.writelines()
-    for lignes in fichier:
-        print(lignes)
-    logging.info('generer_doc')
-
-
 def main():
+    """ This function, is the MAIN function : 
+    This function will check if the argument next or time has been entered by the
+    user:
+
+   If one of the two arguments was entered the program will continue and display
+   the results to the user.
+   If neither of the two arguments was entered by the user the program will
+    display an error message and close.
+    
+    """
     if not args.action:
         print("Error : il manque un argument action ('time' ou 'next')")
-        logging.warning('il manque un argument action')
         return 1
     if args.action == 'time': 
         if not args.station or not args.ligne or not args.destination: 
             print("Error: il manque la ligne et/ou la station et/ou la destination dans les arguments")
-            logging.warning('Error: il manque la ligne et/ou la station et/ou la destination dans les arguments')
             return 1
     if args.action == 'next':
         if not args.station: 
             print("Error: il manque la station dans les arguments")
-            logging.warning('Error: il manque la station dans les arguments')
             return 1
 
     conn = sqlite3.connect('tam.db')
 
     if not conn: # si format ne convient pas, si la base est corrompue...etc
         print("Error : could not connect to database ")
-        logging.warning('Error : could not connect to database')
         return 1
 
     c = conn.cursor()
@@ -146,9 +206,9 @@ def main():
         time_tram('tam.db', c)
     elif args.action == 'next':
         next_tram('tam.db', c)
+        
     conn.commit()
     conn.close()
-    logging.info('fin du programme')
 
     
 
@@ -156,4 +216,4 @@ def main():
 
     
 if __name__ == "__main__":
-    sys.exit(main())
+  sys.exit(main())
